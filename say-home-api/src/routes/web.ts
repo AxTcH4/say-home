cad98d83a875f3b8c39d344e714063966bf28f12
AbderTec <abdtec31@gmail.com>
2026-03-31 05:35:38 +0100

    recherche des proprietes (a remplacer les requetes prisma avec modele matching)

--- say-home-api/src/routes/web.ts
+++ say-home-api/src/routes/web.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import searchRouter from "../search/search.routes.js"
 
 // construct routing tool
 const router = Router();
@@ -7,5 +8,7 @@ const router = Router();
 router.get('/test', (req,res)=> {
     res.json({status:'ok'});
 });
+
+router.use('/search', searchRouter)
 
 export default router;
