634f6aaef538f9c31257f23ee7fe3c58bf558fe6
AbderTec <abdtec31@gmail.com>
2026-04-07 11:21:30 +0100

    wipe old express backend

--- say-home-api/src/index.ts
+++
@@ -1,7 +0,0 @@
-import app from "./app.js" ;
-import dotenv from "dotenv"
-
-dotenv.config();
-
-const PORT= process.env.PORT || 3000;
-app.listen(PORT, ()=>{return console.log(`Server is running on port ${PORT}`)});
--- say-home-api/src/search/search.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as searchController from "./search.controller.js"
-
-const searchRouter = Router();
-
-//search routes
-searchRouter.get('/properties', searchController.searchProperties);
-
-export default searchRouter;
