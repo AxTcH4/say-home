66614dae844f0681cd8d0e0dff96d286e1dbd754
AbderTec <abdtec31@gmail.com>
2026-04-20 20:34:31 +0100

    chore: admin project init

--- say-home-api/src/main/java/ma/sayhome/say_home_api/helpDesk/HelpDeskController.java
+++ say-home-api/src/main/java/ma/sayhome/say_home_api/helpDesk/HelpDeskController.java
@@ -1,9 +1,14 @@
 package ma.sayhome.say_home_api.chatbot;
 
 import ma.sayhome.say_home_api.shared.ControllerBase;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/helpdesk")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ChatbotController extends ControllerBase {
+
 
 }
