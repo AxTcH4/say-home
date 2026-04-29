ade8a7ff881f26e357a6e9f7156e2271f11933c9
Abderrahmane Techa <abdtec31@gmail.com>
2026-04-29 07:53:11 +0100

    Update routes.ts (remove conflict leftovers)

--- say-home-admin/shared/lib/routes.ts
+++ say-home-admin/shared/lib/routes.ts
@@ -1,5 +1,4 @@
 export const APP_ROUTES = {
-<<<<<<< HEAD
     HOME: "/dashboard",
     PROSPECTS: "/prospects",
     PROPERTIES: "/properties",
@@ -9,27 +8,4 @@ export const APP_ROUTES = {
     TICKETS: "/tickets",
     AGENTS: "/agents",
 } as const;
-//   { href: "/dashboard", label: "Dashboard", icon: "/dashboard.svg" },
-//   { href: "/prospects", label: "Prospects", icon: "/prospects.svg" },
-//   { href: "/properties", label: "Properties", icon: "/property.svg" },
-//   { href: "/pipeline", label: "Pipeline", icon: "/pipeline.svg" },
-//   { href: "/ai-matching", label: "AI Matching", icon: "/ai.svg" },
-//   { href: "/appointments", label: "Appointments", icon: "/apt.svg" },
-//   { href: "/tickets", label: "Support Tickets", icon: "/ticket.svg" },
-//   { href: "/agents", label: "Agents", icon: "/agent.svg" },
-// ];
-=======
-  HOME: "/",
-  LOGIN: "/auth/login",
-  // SIGNUP: "/auth/signup",
-  // VERIFY_EMAIL: "/auth/verify-email",
-  FORGOT_PASSWORD: "/auth/forgot-password",
-  RESET_PASSWORD: "/auth/reset-password",
-  // CONTACT: "/contact",
 
-  // DASHBOARD: "/dashboard",
-  PROFILE: "/user/profile",
-  REAL_ESTATE: "/real-estate",
-  // TICKETS: "/tickets",
-} as const;
->>>>>>> 4ac654f (chore: backoffice front end init)
